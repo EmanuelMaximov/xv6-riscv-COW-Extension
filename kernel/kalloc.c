abd113eab7d3cf6cb397cd911f0f84333264947f
Emanuel Maximov <maximov@post.bgu.ac.il>
2022-07-27 18:45:22 +0300

    cow added

---
+++ kernel/kalloc.c
@@ -0,0 +1,133 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+//Ass 3
+extern uint64 cas(volatile void *addr,int excpected, int newval);
+
+//An array with an entry for each physical page
+//reference count per each pyhsical page
+int refs[NUM_PYS_PAGES];
+
+//synchronized array using CAS from the previous assignment
+void
+ref_add(uint64 pa)
+{
+  int old_refs_counter;
+  do {
+    old_refs_counter = refs[PAINDEX(pa)];
+  } while (cas(&(refs[PAINDEX(pa)]),old_refs_counter,old_refs_counter+1));
+}
+
+int
+ref_remove(uint64 pa)
+{
+  int old_refs_counter;
+  do {
+    old_refs_counter = refs[PAINDEX(pa)];
+  } while (cas(&refs[PAINDEX(pa)],old_refs_counter,old_refs_counter-1));
+  return refs[PAINDEX(pa)];
+}
+//
+
+
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+
+  //Ass 3
+  //Init refs array to 0
+  memset(refs, 0, sizeof(int)*NUM_PYS_PAGES);
+  //
+
+
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  //Ass 3
+  //No need to free if the physical page refCount>=1
+  if (ref_remove((uint64)pa) > 0)
+    return;
+
+  refs[PAINDEX((uint64)pa)] = 0;
+  //
+
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r){
+    //Ass 3
+    refs[PAINDEX((uint64)r)] = 1;
+    //
+    kmem.freelist = r->next;
+  }
+    
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
